c0939444a64eaac2c77203026b9e512f1bcf9517
tatsuya <babatsuya@icloud.com>
2023-07-08 21:49:11 +0900

    画像の追加ボタン実装

--- resources/js/Pages/Spots/Create.vue
+++ resources/js/Pages/Spots/Create.vue
@@ -39,6 +39,14 @@ const onFileChange = (e, image) => {
         reader.readAsDataURL(file);
     }
 };
+
+const addImageForm = () => {
+    form.value.images.push({ image_path: null, description: null });
+};
+
+const removeImageForm = (index) => {
+    form.value.images.splice(index, 1);
+};
 </script>
 
 <template>
@@ -94,10 +102,13 @@ const onFileChange = (e, image) => {
                     </div>
                 </div>
 
+                <button type="button" @click="addImageForm" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded">画像を追加</button>
                 <div class="flex flex-wrap -mx-3 mb-6">
                     <div v-for="(image, index) in form.images" :key="index" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                         <div class="my-5">
-                            <label class="mb-2 inline-block text-neutral-700 dark:text-neutral-200" for="image_path">{{ index === 0 ? 'ポジション' : '結果' }}</label>
+                            <label class="mb-2 inline-block text-neutral-700 dark:text-neutral-200" for="image_path">
+                                {{ index === 0 ? '結果' : index === 1 ? 'ポジション' : '追加' + (index - 1) }}
+                            </label>
                             <input
                                 class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
                                 type="file"
@@ -105,6 +116,7 @@ const onFileChange = (e, image) => {
                                 @change="(e) => onFileChange(e, image)"
                             />
                             <img :src="image.image_path" v-if="image.image_path" class="mt-2 w-full h-auto" />
+                            <button v-if="index >= 2" type="button" @click="removeImageForm(index)" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded">削除</button>
                         </div>
 
                         <label for="description" class="block text-sm font-medium text-gray-700">説明</label>
