9102e78d3f9da0bc918ee9026ff71d4dec039c4b
tatsuya <babatsuya@icloud.com>
2023-07-15 16:43:48 +0900

    モーダルにバツボタンを追加

--- resources/js/Components/ImageModal.vue
+++ resources/js/Components/ImageModal.vue
@@ -1,35 +1,34 @@
 <script setup>
-import { ref } from 'vue';
-
-const imageModal = ref({
-    isOpen: false,
-    image: null,
+const props = defineProps({
+    isOpen: {
+        type: Boolean,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    closeModal: {
+        type: Function,
+        required: true,
+    },
 });
-
-const openImageModal = (image) => {
-    imageModal.value.isOpen = true;
-    imageModal.value.image = image;
-};
-
-const closeImageModal = () => {
-    imageModal.value.isOpen = false;
-    imageModal.value.image = null;
-};
 </script>
 
 <template>
-    <div v-if="imageModal.isOpen" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-        <div class="flex items-end justify-center min-h-screen">
-            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" @click="closeImageModal"></div>
-            <div class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
-                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <div class="sm:flex sm:items-start">
-                        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                            <img :src="imageModal.image" alt="" />
-                        </div>
+    <div v-if="isOpen" class="fixed z-10 inset-0 overflow-y-auto flex items-center justify-center" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" @click="closeModal"></div>
+        <div class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-7/10 relative">
+            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div class="sm:flex sm:items-start">
+                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <img :src="image" alt="" class="w-full" />
                     </div>
                 </div>
             </div>
         </div>
+        <v-btn icon class="absolute top-[-310px] right-8" @click="closeModal">
+            <v-icon>mdi-close</v-icon>
+        </v-btn>
     </div>
 </template>
--- resources/js/Pages/Spots/Show.vue
+++ resources/js/Pages/Spots/Show.vue
@@ -1,6 +1,7 @@
 <script setup>
 import NavBar from '@/Components/NavBar.vue';
-import { defineProps, onMounted } from 'vue';
+import ImageModal from '@/Components/ImageModal.vue';
+import { defineProps, ref } from 'vue';
 
 const props = defineProps({
     spot: {
@@ -8,6 +9,21 @@ const props = defineProps({
         required: true,
     },
 });
+
+const imageModal = ref({
+    isOpen: false,
+    image: null,
+});
+
+const openImageModal = (image) => {
+    imageModal.value.isOpen = true;
+    imageModal.value.image = image;
+};
+
+const closeImageModal = () => {
+    imageModal.value.isOpen = false;
+    imageModal.value.image = null;
+};
 </script>
 <template>
     <v-app id="inspire">
@@ -18,10 +34,11 @@ const props = defineProps({
                 <h2>{{ spot.description }}</h2>
                 <div class="flex flex-wrap space-4">
                     <div v-for="(image, index) in spot.images" :key="index" class="w-1/4">
-                        <img class="object-cover" :src="image.image_path" alt="" />
+                        <img class="object-cover cursor-pointer" :src="image.image_path" alt="" @click="openImageModal(image.image_path)" />
                     </div>
                 </div>
             </div>
+            <ImageModal :isOpen="imageModal.isOpen" :image="imageModal.image" :closeModal="closeImageModal" />
         </v-main>
     </v-app>
 </template>
