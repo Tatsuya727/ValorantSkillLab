b9413064fdc9067107a26dfb6c9bf8c9ddb35ab1
tatsuya <babatsuya@icloud.com>
2023-07-15 19:50:41 +0900

    updateOrderメソッド追加

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Image;
 
 class ImageController extends Controller
 {
-    //
+    public function updateOrder(Request $request)
+    {
+        $images = $request->input('images');
+
+        foreach ($images as $order => $id) {
+            Image::where('id', $id)->update(['order' => $order]);
+        }
+        
+        return response()->json(['status' => 'success']);
+    }
 }
