3644055af7068cf5d08212f6226d66903d390995
tatsuya <babatsuya@icloud.com>
2023-07-18 20:09:21 +0900

    画像が横並びになるように修正

--- resources/js/Pages/Spots/Index.vue
+++ resources/js/Pages/Spots/Index.vue
@@ -22,40 +22,53 @@ const props = defineProps({
             <v-container>
                 <v-row v-if="props.spots">
                     <template v-for="(category, index) in props.categories" :key="category.id">
-                        <v-col class="mt-2" cols="12">
-                            <strong>{{ category.name }}</strong>
+                        <v-col class="mt-4" cols="12">
+                            <h4 class="text-2xl font-bold">{{ category.name }}</h4>
                         </v-col>
-                        <v-col class="mt-2" cols="12">
-                            <div class="flex mx-auto p-10 gap-4">
-                                <div v-for="spot in props.spots" :key="spot.id" class="w-1/6 bg-cyan-400">
-                                    <div v-if="spot.category_id === category.id">
-                                        <div v-for="(image, index) in spot.images" :key="index" class="bg-green-300">
-                                            <div v-if="index === 0">
-                                                <p>{{ spot.description }}</p>
-                                                <Link :href="spot.show_url">
-                                                    <img class="w-full h-auto" :src="image.image_path" alt="" />
-                                                </Link>
-                                            </div>
+                        <v-col cols="12">
+                            <div class="flex flex-wrap mx-auto p-10 gap-4">
+                                <template v-for="spot in props.spots" :key="spot.id">
+                                    <div v-if="spot.category_id === category.id" class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-3/12 mb-4 bg-white rounded shadow">
+                                        <Link :href="spot.show_url">
+                                            <img class="w-full h-auto rounded-t" :src="spot.images[0].image_path" alt="" />
+                                        </Link>
+                                        <div class="p-4">
+                                            <p class="text-sm text-gray-700">{{ spot.description }}</p>
                                         </div>
                                     </div>
-                                </div>
+                                </template>
                             </div>
                         </v-col>
                     </template>
-                    <!-- <template v-for="(spot, index) in props.spots" :key="spot.id">
-                        <v-col class="mt-2" cols="12">
-                            <strong>Category {{ index + 1 }}</strong>
-                        </v-col>
-                        <div class="flex-auto" v-for="(image, index) in spot.images" :key="index">
-                            <h1>{{ spot.id }}</h1>
-                            <p v-if="index === 0">{{ spot.description }}</p>
-                            <Link :href="spot.show_url">
-                                <img class="w-1/4" :src="image.image_path" alt="" v-if="index === 0" />
-                            </Link>
-                        </div>
-                    </template> -->
                 </v-row>
             </v-container>
         </v-main>
     </v-app>
 </template>
+
+<!-- <template>
+    <v-app id="inspire">
+        <NavBar />
+        <v-main class="bg-grey-lighten-2">
+            <v-container>
+                <v-row v-if="props.spots">
+                    <strong>Category 1</strong>
+                    <v-col class="mt-2" cols="12">
+                        <div class="flex mx-auto p-10 gap-4">
+                            <div v-for="(spot, index) in props.spots" :key="spot.id" class="w-1/6 bg-cyan-400">
+                                <div v-for="(image, index) in spot.images" :key="index" class="bg-green-300">
+                                    <div v-if="index === 0">
+                                        <p>{{ spot.description }}</p>
+                                        <Link :href="spot.show_url">
+                                            <img class="w-full h-auto" :src="image.image_path" alt="" />
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
+</template> -->
