6614e3cee7f79398aff0b4cf35f9a02d173ae295
tatsuya <babatsuya@icloud.com>
2023-07-18 20:58:18 +0900

    カテゴリー名で折りたためるように変更

--- resources/js/Pages/Spots/Index.vue
+++ resources/js/Pages/Spots/Index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import NavBar from '@/Components/NavBar.vue';
-import { defineProps } from 'vue';
+import { defineProps, reactive } from 'vue';
 import { Link } from '@inertiajs/inertia-vue3';
 
 const props = defineProps({
@@ -13,6 +13,19 @@ const props = defineProps({
         required: true,
     },
 });
+
+// 各カテゴリーの表示状態を管理するオブジェクト
+const showCategory = reactive({});
+
+// 初期状態では全てのカテゴリーを表示する
+props.categories.forEach((category) => {
+    showCategory[category.id] = true;
+});
+
+// カテゴリーの表示状態を切り替える関数
+const toggleCategory = (categoryId) => {
+    showCategory[categoryId] = !showCategory[categoryId];
+};
 </script>
 
 <template>
@@ -23,25 +36,30 @@ const props = defineProps({
                 <v-row v-if="props.spots">
                     <template v-for="(category, index) in props.categories" :key="category.id">
                         <v-col class="mt-4" cols="12">
-                            <h4 class="text-2xl font-bold">{{ category.name }}</h4>
+                            <h4 class="text-2xl font-bold cursor-pointer" @click="toggleCategory(category.id)">
+                                <v-icon icon="mdi-chevron-down" v-if="showCategory[category.id]"></v-icon>
+                                <v-icon icon="mdi-chevron-right" v-else></v-icon>
+                                {{ category.name }}
+                            </h4>
                         </v-col>
-                        <v-col cols="12">
-                            <div class="flex flex-wrap mx-auto gap-4">
-                                <v-slide-group v-model="model" class="" selected-class="bg-success" show-arrows>
-                                    <v-slide-group-item v-for="spot in props.spots" :key="spot.id">
-                                        <div v-if="spot.category_id === category.id" class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-2/12 mb-4 bg-white rounded shadow mr-5">
-                                            <!-- <div v-if="spot.category_id === category.id" class="min-w-full sm:min-w-1/2 md:min-w-1/3 lg:min-w-1/4 xl:min-w-2/12 mb-4 bg-white rounded shadow mr-5"> -->
-                                            <Link :href="spot.show_url">
-                                                <img class="w-full h-auto rounded-t" :src="spot.images[0].image_path" alt="" />
-                                            </Link>
-                                            <div class="p-4">
-                                                <p class="text-sm text-gray-700">{{ spot.description }}</p>
+                        <v-expand-transition>
+                            <v-col cols="12" v-if="showCategory[category.id]">
+                                <div class="flex flex-wrap mx-auto gap-4">
+                                    <v-slide-group v-model="model" class="" selected-class="bg-success" show-arrows>
+                                        <v-slide-group-item v-for="spot in props.spots" :key="spot.id">
+                                            <div v-if="spot.category_id === category.id" class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-2/12 mb-4 bg-white rounded shadow mr-5">
+                                                <Link :href="spot.show_url">
+                                                    <img class="w-full h-auto rounded-t" :src="spot.images[0].image_path" alt="" />
+                                                </Link>
+                                                <div class="p-4">
+                                                    <p class="text-sm text-gray-700">{{ spot.description }}</p>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </v-slide-group-item>
-                                </v-slide-group>
-                            </div>
-                        </v-col>
+                                        </v-slide-group-item>
+                                    </v-slide-group>
+                                </div>
+                            </v-col>
+                        </v-expand-transition>
                     </template>
                 </v-row>
             </v-container>
