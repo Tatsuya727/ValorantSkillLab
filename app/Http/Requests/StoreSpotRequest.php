90e43e8149e1b0e0a877728fa331908576536aa5
tatsuya <babatsuya@icloud.com>
2023-07-26 16:02:03 +0900

    エラー文を設定

--- app/Http/Requests/StoreSpotRequest.php
+++ app/Http/Requests/StoreSpotRequest.php
@@ -31,4 +31,20 @@ class StoreSpotRequest extends FormRequest
             'tags' =>         ['array', 'max:3'],
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'title.requred' => 'タイトルは必須です',
+            'title.max' => 'タイトルは150文字以内で入力してください',
+            'description.max' => '説明は255文字以内で入力してください',
+            'map_id.required' => 'マップは必須です',
+            'map_id.exists' => 'マップが存在しません',
+            'character_id.required' => 'キャラクターは必須です',
+            'character_id.exists' => 'キャラクターが存在しません',
+            'images.*.image_path.required' => '画像は必須です',
+            'images.*.description.max' => '画像の説明は255文字以内で入力してください',
+            'tags.max' => 'タグは最大3つまで選択できます',
+        ];
+    }
 }
