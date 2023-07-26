d42e1a151350259323827a4cac0932bfc4aca0cd
tatsuya <babatsuya@icloud.com>
2023-07-26 16:17:21 +0900

    descriptionを必須に変更

--- app/Http/Requests/StoreSpotRequest.php
+++ app/Http/Requests/StoreSpotRequest.php
@@ -23,7 +23,7 @@ class StoreSpotRequest extends FormRequest
     {
         return [
             'title' =>        ['required', 'max:150'],
-            'description' =>  [ 'max:255'],
+            'description' =>  ['required','max:255'],
             'map_id' =>       ['required', 'exists:maps,id', 'integer'],
             'character_id' => ['required', 'exists:characters,id', 'integer'],
             'images.*.image_path' =>  ['required'],
@@ -35,14 +35,14 @@ class StoreSpotRequest extends FormRequest
     public function messages(): array
     {
         return [
-            'title.requred' => 'タイトルは必須です',
+            'title.required' => 'タイトルは必須です',
             'title.max' => 'タイトルは150文字以内で入力してください',
             'description.max' => '説明は255文字以内で入力してください',
-            'map_id.required' => 'マップは必須です',
+            'map_id.required' => 'マップを選択してください',
             'map_id.exists' => 'マップが存在しません',
-            'character_id.required' => 'キャラクターは必須です',
+            'character_id.required' => 'キャラクターを選択してください',
             'character_id.exists' => 'キャラクターが存在しません',
-            'images.*.image_path.required' => '画像は必須です',
+            'images.*.image_path.required' => '画像を選択してください',
             'images.*.description.max' => '画像の説明は255文字以内で入力してください',
             'tags.max' => 'タグは最大3つまで選択できます',
         ];
