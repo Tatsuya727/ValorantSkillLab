646a5d5185dbc7bd81a794ff85a9a6a27ab9dac4
tatsuya <babatsuya@icloud.com>
2023-07-31 22:47:34 +0900

    サイドメニューバーを常時表示に変更

--- resources/js/Components/NavBar.vue
+++ resources/js/Components/NavBar.vue
@@ -3,12 +3,36 @@ import { ref } from 'vue';
 import { Link } from '@inertiajs/inertia-vue3';
 
 const drawer = ref(null);
+const cards = ['Today', 'Yesterday'];
+const links = [
+    ['mdi-inbox-arrow-down', 'Inbox'],
+    ['mdi-send', 'Send'],
+    ['mdi-delete', 'Trash'],
+    ['mdi-alert-octagon', 'Spam'],
+];
 </script>
 
 <template>
-    <v-app-bar>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-navigation-drawer v-model="drawer">
+        <v-sheet color="grey-lighten-4" class="pa-4">
+            <v-avatar class="mb-4" color="grey-darken-1" size="64"></v-avatar>
 
+            <div>john@google.com</div>
+        </v-sheet>
+
+        <v-divider></v-divider>
+
+        <v-list>
+            <v-list-item v-for="[icon, text] in links" :key="icon" link>
+                <template v-slot:prepend>
+                    <v-icon>{{ icon }}</v-icon>
+                </template>
+
+                <v-list-item-title>{{ text }}</v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+    <v-app-bar>
         <v-toolbar-title>ValorantSkillLab</v-toolbar-title>
 
         <Link href="/" class="mr-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Home</Link>
@@ -19,8 +43,4 @@ const drawer = ref(null);
 
         <v-spacer></v-spacer>
     </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" temporary>
-        <div>サイドメニュー</div>
-    </v-navigation-drawer>
 </template>
