b9a89493608ca7928ae2fe75d3b7307c2770c7c4
tatsuya <babatsuya@icloud.com>
2023-08-02 00:16:15 +0900

    タグのhoverアニメーションを追加

--- resources/js/Pages/Spots/Index.vue
+++ resources/js/Pages/Spots/Index.vue
@@ -286,7 +286,7 @@ const resetSpots = () => {
                                                                 :key="index"
                                                                 @click="filterSpotsByTag(tag.name)"
                                                                 :class="{
-                                                                    'border-2 border-cyan-500 rounded-lg px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 cursor-pointer hover:text-white':
+                                                                    'border-2 border-cyan-500 rounded-lg px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 cursor-pointer hover:text-white hover:bg-cyan-500':
                                                                         !selectedTag.value,
                                                                     'bg-cyan-500 text-white': selectedTag && selectedTag === tag.name,
                                                                 }"
