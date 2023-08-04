8fb62f129c79490607d4bdc5e857ca66556f11e5
tatsuya <babatsuya@icloud.com>
2023-08-05 00:35:44 +0900

    google login controller 作成

---
+++ app/Http/Controllers/GoogleLoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Hash;
+use Inertia\Inertia;
+
+class GoogleLoginController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+            $socialiteUser = Socialite::driver('google')->user();
+            $email = $socialiteUser->email;
+
+            $user = User::firstorCreate(
+                ['email' => $email],
+                ['name' => $socialiteUser->name,]
+            );
+
+            Auth::login($user, true);
+
+            return Inertia::render('spots/Index');
+        } catch (Exception $e) {
+            Log::error($e);
+            throw $e;
+        }
+    }
+}
