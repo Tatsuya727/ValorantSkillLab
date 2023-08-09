8fff3fb995af53982bcce37f85d0633df92a3d54
tatsuya <babatsuya@icloud.com>
2023-08-09 20:09:29 +0900

    タグを選択してもLinkタグのリンクに飛ばないように修正

--- resources/js/Components/original/ShareSpotList.vue
+++ resources/js/Components/original/ShareSpotList.vue
@@ -65,30 +65,28 @@ const filterSpotsByTag = (tag) => {
 </script>
 
 <template>
-    <div v-for="spot in props.spots" :key="spot.id" class="bg-zinc-900 m-10 text-white rounded-lg shadow-lg">
-        <Link :href="spot.show_url" class="flex">
-            <div>
-                <img :width="300" cover class="rounded-l-lg" :src="spot.images[0].image_path" alt="サムネイル画像" loading="lazy" />
-            </div>
-
-            <div class="p-4">
-                <h1 class="text-2xl font-bold">{{ spot.title }}</h1>
-                <div class="mt-2">
-                    <p class="text-sm text-gray-400">
-                        map: <span class="font-bold text-white">{{ spot.map.name }}</span>
-                    </p>
-                    <p class="text-sm text-gray-400">
-                        character: <span class="font-bold text-white">{{ spot.character.name }}</span>
-                    </p>
-                </div>
+    <div v-for="spot in props.spots" :key="spot.id" class="m-10 text-white">
+        <div class="flex">
+            <Link :href="spot.show_url">
+                <div><img :width="300" cover class="rounded-l-lg" :src="spot.images[0].image_path" alt="サムネイル画像" loading="lazy" /></div>
+            </Link>
+            <div class="p-4 bg-zinc-900 rounded w-full">
+                <Link :href="spot.show_url">
+                    <h1 class="text-2xl font-bold">{{ spot.title }}</h1>
+                    <div class="mt-2">
+                        <p class="text-sm text-gray-400">
+                            map: <span class="font-bold text-white">{{ spot.map.name }}</span>
+                        </p>
+                        <p class="text-sm text-gray-400">
+                            character: <span class="font-bold text-white">{{ spot.character.name }}</span>
+                        </p>
+                    </div>
+                </Link>
                 <!-- タグの名前をすべて表示 -->
                 <div class="flex flex-wrap mt-4">
                     <div v-for="(tag, index) in spot.tags" :key="index">
                         <div
-                            @click="
-                                filterSpotsByTag(tag.name);
-                                $event.stopPropagation();
-                            "
+                            @click="filterSpotsByTag(tag.name)"
                             :class="{
                                 'border-2 border-cyan-500 rounded-lg px-3 py-1 text-sm font-semibold text-white mr-2 mb-2 cursor-pointer hover:text-white hover:bg-cyan-500': !selectedTag.value,
                                 'bg-cyan-500 text-white': selectedTag && selectedTag === tag.name,
@@ -99,6 +97,6 @@ const filterSpotsByTag = (tag) => {
                     </div>
                 </div>
             </div>
-        </Link>
+        </div>
     </div>
 </template>
