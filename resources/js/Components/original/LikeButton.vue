09606c1e9708dadeb4baa9286bbfa2694a979f27
tatsuya <babatsuya@icloud.com>
2023-08-11 21:45:24 +0900

    いいね削除機能追加

--- resources/js/Components/original/LikeButton.vue
+++ resources/js/Components/original/LikeButton.vue
@@ -9,29 +9,25 @@ const props = defineProps({
     },
 });
 
-const isLiked = ref(false);
-
 // いいね機能
-const toggleLike = () => {
-    if (isLiked.value) {
+const toggleLike = (spot) => {
+    if (spot.is_liked_by_user) {
         Inertia.delete(route('likes.destroy', props.spot.id));
-        isLiked.value = false;
     } else {
         Inertia.post(route('likes.store', props.spot.id));
-        isLiked.value = true;
     }
 };
 </script>
 
 <template>
     <div class="text-white">
-        <div @click="toggleLike">
+        <div @click="toggleLike(spot)">
             <template v-if="spot.is_liked_by_user">
-                <v-icon>mdi-heart</v-icon>
+                <v-icon class="cursor-pointer">mdi-heart</v-icon>
                 {{ spot.liked_by_count }}
             </template>
             <template v-else>
-                <v-icon>mdi-heart-outline</v-icon>
+                <v-icon class="cursor-pointer">mdi-heart-outline</v-icon>
                 {{ spot.liked_by_count }}
             </template>
         </div>
