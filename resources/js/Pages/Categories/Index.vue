9750f60df00a620e56fbb97f410daa927ee77a65
tatsuya <babatsuya@icloud.com>
2023-08-20 16:38:48 +0900

    スポットがない場合、画像の代わりに灰色のカードを表示する

--- resources/js/Pages/Categories/Index.vue
+++ resources/js/Pages/Categories/Index.vue
@@ -37,15 +37,15 @@ const getFirstSpotForCategory = (categoryId) => {
         <NavBar />
         <v-main class="bg-zinc-900">
             <v-container fluid>
-                <v-row class="bg-neutral-700 pt-5 pb-10 mt-3 mx-3 rounded">
+                <v-row class="bg-neutral-700 pt-5 pb-10 mt-2 rounded">
                     <v-col v-for="category in props.categories" :key="category.id" cols="12" sm="6" md="4" lg="3" class="mt-10">
                         <!-- そのカテゴリーを持つ最初のspotを表示 -->
-                        <div class="ml-4 cursor-pointer d-flex align-items-center" @click="selectCategory(category)">
+                        <div class="ml-4 cursor-pointer text-center" @click="selectCategory(category)">
                             <template v-if="getFirstSpotForCategory(category.id)">
-                                <v-img width="300px" :src="getFirstSpotForCategory(category.id)" class="mr-4 rounded"></v-img>
+                                <v-img :src="getFirstSpotForCategory(category.id)" class="min-w-full min-h-52 mr-4 rounded"></v-img>
                             </template>
                             <template v-else>
-                                <v-card class="mr-4 bg-gray-400" style="width: 300px; height: 169px"></v-card>
+                                <div class="w-full h-52 bg-gray-400 mr-4 rounded"></div>
                             </template>
                             <div class="cursor-pointer">{{ category.name }}</div>
                         </div>
