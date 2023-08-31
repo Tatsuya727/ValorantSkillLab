a716f28b9906c57f1c33c20a43c076a5d671aecc
tatsuya <babatsuya@icloud.com>
2023-08-31 15:04:02 +0900

    表示方法を縦と横に切り替える機能完成

--- resources/js/Components/original/ShareSpotList.vue
+++ resources/js/Components/original/ShareSpotList.vue
@@ -60,6 +60,15 @@ const handleOrderBy = (item) => {
     Inertia.get(route('sharespots.index', { orderBy: orderBy.value }));
 };
 
+const isVertical = ref(true);
+
+const setVertical = () => {
+    isVertical.value = true;
+};
+
+const setHorizontal = () => {
+    isVertical.value = false;
+};
 const orderItems = [
     {
         title: 'デフォルト',
@@ -95,7 +104,7 @@ const orderItems = [
                 件
             </div>
         </div>
-        <div class="mr-5">
+        <div class="mr-5 flex">
             <v-menu>
                 <template v-slot:activator="{ props }">
                     <v-btn color="" v-bind="props"><v-icon>mdi-sort-variant</v-icon>並べ替え</v-btn>
@@ -106,12 +115,75 @@ const orderItems = [
                     </v-list-item>
                 </v-list>
             </v-menu>
+            <div class="ml-8 mt-2">
+                <v-icon @click="setHorizontal" class="text-white cursor-pointer mr-3">mdi-apps</v-icon>
+                <v-icon @click="setVertical" class="text-white cursor-pointer">mdi-menu</v-icon>
+            </div>
         </div>
     </div>
 
     <Pagination class="mt-5 text-white" :links="props.spots.links"></Pagination>
 
-    <v-row justify="center" class="mx-3">
+    <v-row v-if="isVertical" justify="center" class="mx-3">
+        <v-col v-for="spot in props.spots.data" :key="spot.id" cols="10" sm="12" md="10" lg="10" class="mt-10">
+            <div class="flex flex-row rounded relative h-full">
+                <!-- 画像 -->
+                <Link :href="spot.show_url" class="flex-1">
+                    <v-img cover class="cursor-pointer h-full w-full" :src="spot.images[0].image_path" alt="サムネイル画像" loading="lazy"></v-img>
+                </Link>
+                <!-- 情報 -->
+                <div class="p-4 bg-neutral-800 flex-1">
+                    <div class="absolute top-50 right-0 mr-2 flex cursor-pointer">
+                        <ShareSpotMenu :spot="spot" :userCategories="userCategories" :flash="flash" />
+                    </div>
+                    <div class="mb-4">
+                        <v-tooltip :text="spot.title" location="top">
+                            <template v-slot:activator="{ props }">
+                                <h2 v-bind="props" class="text-gray-700 text-white spot-title white-space-normal">
+                                    {{ spot.title }}
+                                </h2>
+                            </template>
+                        </v-tooltip>
+                        <p class="text-sm text-gray-700 text-grey">
+                            map:
+                            <span
+                                :class="{
+                                    'font-bold text-white map-name cursor-pointer hover:underline': !selectedMap,
+                                    'font-bold text-orange-400 map-name cursor-pointer hover:underline': selectedMap,
+                                }"
+                                @click="filterMap(spot.map)"
+                                >{{ spot.map.name }}</span
+                            >
+                        </p>
+                        <p class="text-sm text-gray-700 text-grey">
+                            character:
+                            <span
+                                :class="{
+                                    'font-bold text-white character-name cursor-pointer hover:underline': !selectedCharacter,
+                                    'font-bold text-orange-400 character-name cursor-pointer hover:underline': selectedCharacter,
+                                }"
+                                @click="filterCharacter(spot.character)"
+                            >
+                                {{ spot.character.name }}
+                            </span>
+                        </p>
+                        <!-- タグの名前をすべて表示 -->
+                        <div class="flex flex-wrap">
+                            <LikeButton class="mt-2 mr-2" :spot="spot" />
+                            <SpotTags :tags="spot.tags" :selectedTag="selectedTag" :selectedMap="props.selectedMap" :selectedCharacter="props.selectedCharacter" :routeName="'sharespots.index'" />
+                        </div>
+                    </div>
+                    <div class="absolute bottom-0 right-0 mb-2 mr-2 flex justify-between">
+                        <div class="text-gray-400 mx-3 text-sm">{{ dayjs(spot.created_at).format('YYYY年MM月DD日') }}</div>
+                        <!-- ユーザーネーム -->
+                        <div class="text-green">{{ spot.user.name }}</div>
+                    </div>
+                </div>
+            </div>
+        </v-col>
+    </v-row>
+
+    <v-row v-else justify="center" class="mx-3">
         <v-col v-for="spot in props.spots.data" :key="spot.id" cols="12" sm="6" md="4" lg="3" class="mt-10">
             <div class="flex flex-col rounded overflow-hidden relative h-full">
                 <!-- 画像 -->
