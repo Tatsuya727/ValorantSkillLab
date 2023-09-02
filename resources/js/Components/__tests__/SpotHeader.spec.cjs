547cc3a1528e41b5c1e382b85047d2c9504fa91c
tatsuya <babatsuya@icloud.com>
2023-09-02 17:14:21 +0900

    spot headerのテスト

---
+++ resources/js/Components/__tests__/SpotHeader.spec.cjs
@@ -0,0 +1,55 @@
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import SpotHeader from '../original/SpotHeader.vue';
+
+describe('SpotHeader.vue', () => {
+    const propsData = {
+        maps: [],
+        characters: [],
+        tags: [],
+        categories: [],
+        search: '',
+        selectedTag: 'testTag', // 'testTag' is not a valid value for prop 'selectedTag
+        selectedMap: {
+            name: 'testMap',
+        },
+        selectedCharacter: {
+            name: 'testCharacter',
+        },
+        selectedCategory: {
+            name: 'testCategory',
+        },
+    };
+
+    const isMobile = false;
+
+    const wrapper = mount(SpotHeader, { propsData });
+
+    it('検索ボタンが存在する', () => {
+        expect(wrapper.find('.search-button').exists()).toBe(true);
+    });
+
+    it('リセットボタンが存在する', () => {
+        expect(wrapper.find('v-btn[color="red"]').exists()).toBe(true);
+    });
+
+    it('検索フィールドが存在する', () => {
+        expect(wrapper.find('v-text-field[data-test="search-input"]').exists()).toBe(true);
+    });
+
+    it('マップの絞り込みが正しく表示される', () => {
+        expect(wrapper.find('v-chip.selected-map').text()).toBe(propsData.selectedMap.name);
+    });
+
+    it('キャラクターの絞り込みが正しく表示される', () => {
+        expect(wrapper.find('v-chip.selected-character').text()).toBe(propsData.selectedCharacter.name);
+    });
+
+    it('タグの絞り込みが正しく表示される', () => {
+        expect(wrapper.find('v-chip.selected-tag').text()).toBe(propsData.selectedTag);
+    });
+
+    it('絞り込みボタンが存在する', () => {
+        expect(wrapper.find('v-btn.filtering-btn').exists()).toBe(true);
+    });
+});
