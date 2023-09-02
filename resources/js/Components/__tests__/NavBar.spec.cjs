4ee66b84f3cf978f83b638c45eb5df71718222f9
tatsuya <babatsuya@icloud.com>
2023-09-02 15:08:43 +0900

    ログインしている場合としていない場合のログイン、ログアウトボタンのテスト

--- resources/js/Components/__tests__/NavBar.spec.cjs
+++ resources/js/Components/__tests__/NavBar.spec.cjs
@@ -1,7 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import NavBar from '../original/NavBar.vue';
-import { Inertia } from '@inertiajs/inertia';
 
 describe('NavBar.vue', () => {
     global.route = (name, params = {}) => {
@@ -38,11 +37,12 @@ describe('NavBar.vue', () => {
 
     const isMobile = false;
 
-    it('正しいタイトル', () => {
-        console.log(wrapper.html());
+    it('タイトルの文字とリンクが正しい', () => {
         const title = wrapper.find('v-toolbar-title');
 
         expect(title.text()).toBe('ValorantSkillLab');
+
+        expect(title.find('a').attributes('href')).toBe('/');
     });
 
     it('サイドメニューのリンクが正しい', () => {
@@ -63,44 +63,94 @@ describe('NavBar.vue', () => {
     });
 });
 
-// describe('ログインしていない場合', () => {
-//     it('ログインボタンと新規登録ボタンが表示される', () => {
-//         const wrapper = setup();
+describe('ログインしていない場合', () => {
+    global.route = (name, params = {}) => {
+        // パラメータが存在する場合、それをURLに追加します。
+        let paramString = Object.entries(params)
+            .map(([key, val]) => `${key}=${val}`)
+            .join('&');
+        if (paramString) {
+            paramString = `?${paramString}`;
+        }
+        return `mocked-route-for-${name}${paramString}`;
+    };
 
-//         expect(wrapper.find('a[href="route(\'login\')"]').exists()).toBeTruthy();
-//         expect(wrapper.find('a[href="route(\'register\')"]').exists()).toBeTruthy();
-//     });
+    const wrapper = mount(NavBar, {
+        global: {
+            mocks: {
+                $page: {
+                    props: {
+                        auth: {
+                            user: null,
+                        },
+                        NavCategories: [],
+                    },
+                },
+                route: global.route,
+            },
+        },
+    });
 
-//     it('ログアウトボタンが表示されない', () => {
-//         const wrapper = setup();
+    const isMobile = false;
 
-//         expect(wrapper.find('a[href="route(\'logout\')"]').exists()).toBeFalsy();
-//     });
-// });
+    it('ログインボタンと新規登録ボタンが表示される', () => {
+        const loginButton = wrapper.find('a[href="mocked-route-for-login');
+        const registerButton = wrapper.find('a[href="mocked-route-for-register');
 
-// describe('ログインしている場合', () => {
-//     it('ログアウトボタンが表示される', () => {
-//         const loggedInUser = {
-//             name: 'test',
-//         };
+        expect(loginButton.exists()).toBeTruthy();
+        expect(registerButton.exists()).toBeTruthy();
 
-//         const wrapper = setup({
-//             user: loggedInUser,
-//         });
+        expect(loginButton.text()).toBe('ログイン');
+        expect(registerButton.text()).toBe('新規登録');
+    });
 
-//         expect(wrapper.find('a[href="route(\'logout\')"]').exists()).toBeTruthy();
-//     });
+    it('ログアウトボタンが表示されない', () => {
+        expect(wrapper.find('a[href="mocked-route-for-logout').exists()).toBeFalsy();
+    });
+});
 
-//     it('ログインボタンと新規登録ボタンが表示されない', () => {
-//         const loggedInUser = {
-//             name: 'test',
-//         };
+describe('ログインしている場合', () => {
+    global.route = (name, params = {}) => {
+        // パラメータが存在する場合、それをURLに追加します。
+        let paramString = Object.entries(params)
+            .map(([key, val]) => `${key}=${val}`)
+            .join('&');
+        if (paramString) {
+            paramString = `?${paramString}`;
+        }
+        return `mocked-route-for-${name}${paramString}`;
+    };
 
-//         const wrapper = setup({
-//             user: loggedInUser,
-//         });
+    const testUser = {
+        id: 1,
+        name: 'test',
+    };
 
-//         expect(wrapper.find('a[href="route(\'login\')"]').exists()).toBeFalsy();
-//         expect(wrapper.find('a[href="route(\'register\')"]').exists()).toBeFalsy();
-//     });
-// });
+    const wrapper = mount(NavBar, {
+        global: {
+            mocks: {
+                $page: {
+                    props: {
+                        auth: {
+                            user: testUser,
+                        },
+                        NavCategories: [],
+                    },
+                },
+                route: global.route,
+            },
+        },
+    });
+
+    const isMobile = false;
+
+    it('ログインボタンと新規登録ボタンが表示されない', () => {
+        console.log(wrapper.html());
+        expect(wrapper.find('a[href="mocked-route-for-login').exists()).toBeFalsy();
+        expect(wrapper.find('a[href="mocked-route-for-register').exists()).toBeFalsy();
+    });
+
+    it('ログアウトボタンが表示される', () => {
+        expect(wrapper.find('a[href="mocked-route-for-logout').exists()).toBeTruthy();
+    });
+});
