14c2b261048dd5a445fccd534fd4e339035560a4
tatsuya <babatsuya@icloud.com>
2023-09-02 15:12:51 +0900

    モックを1つにまとめる

--- resources/js/Components/__tests__/NavBar.spec.cjs
+++ resources/js/Components/__tests__/NavBar.spec.cjs
@@ -2,40 +2,38 @@ import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import NavBar from '../original/NavBar.vue';
 
-describe('NavBar.vue', () => {
-    global.route = (name, params = {}) => {
-        // パラメータが存在する場合、それをURLに追加します。
-        let paramString = Object.entries(params)
-            .map(([key, val]) => `${key}=${val}`)
-            .join('&');
-        if (paramString) {
-            paramString = `?${paramString}`;
-        }
-        return `mocked-route-for-${name}${paramString}`;
-    };
+const mockRoute = (name, params = {}) => {
+    const paramString = Object.entries(params)
+        .map(([key, val]) => `${key}=${val}`)
+        .join('&');
+    return `mocked-route-for-${name}${paramString ? '?' + paramString : ''}`;
+};
 
+const createWrapper = (user) => {
+    return mount(NavBar, {
+        global: {
+            mocks: {
+                $page: {
+                    props: {
+                        auth: { user },
+                        NavCategories: [],
+                    },
+                },
+                route: mockRoute,
+            },
+        },
+    });
+};
+
+const isMobile = false;
+
+describe('NavBar.vue', () => {
     const testUser = {
         id: 1,
         name: 'test',
     };
 
-    const wrapper = mount(NavBar, {
-        global: {
-            mocks: {
-                $page: {
-                    props: {
-                        auth: {
-                            user: testUser,
-                        },
-                        NavCategories: [],
-                    },
-                },
-                route: global.route,
-            },
-        },
-    });
-
-    const isMobile = false;
+    const wrapper = createWrapper(testUser);
 
     it('タイトルの文字とリンクが正しい', () => {
         const title = wrapper.find('v-toolbar-title');
@@ -64,34 +62,7 @@ describe('NavBar.vue', () => {
 });
 
 describe('ログインしていない場合', () => {
-    global.route = (name, params = {}) => {
-        // パラメータが存在する場合、それをURLに追加します。
-        let paramString = Object.entries(params)
-            .map(([key, val]) => `${key}=${val}`)
-            .join('&');
-        if (paramString) {
-            paramString = `?${paramString}`;
-        }
-        return `mocked-route-for-${name}${paramString}`;
-    };
-
-    const wrapper = mount(NavBar, {
-        global: {
-            mocks: {
-                $page: {
-                    props: {
-                        auth: {
-                            user: null,
-                        },
-                        NavCategories: [],
-                    },
-                },
-                route: global.route,
-            },
-        },
-    });
-
-    const isMobile = false;
+    const wrapper = createWrapper(null);
 
     it('ログインボタンと新規登録ボタンが表示される', () => {
         const loginButton = wrapper.find('a[href="mocked-route-for-login');
@@ -110,39 +81,12 @@ describe('ログインしていない場合', () => {
 });
 
 describe('ログインしている場合', () => {
-    global.route = (name, params = {}) => {
-        // パラメータが存在する場合、それをURLに追加します。
-        let paramString = Object.entries(params)
-            .map(([key, val]) => `${key}=${val}`)
-            .join('&');
-        if (paramString) {
-            paramString = `?${paramString}`;
-        }
-        return `mocked-route-for-${name}${paramString}`;
-    };
-
     const testUser = {
         id: 1,
         name: 'test',
     };
 
-    const wrapper = mount(NavBar, {
-        global: {
-            mocks: {
-                $page: {
-                    props: {
-                        auth: {
-                            user: testUser,
-                        },
-                        NavCategories: [],
-                    },
-                },
-                route: global.route,
-            },
-        },
-    });
-
-    const isMobile = false;
+    const wrapper = createWrapper(testUser);
 
     it('ログインボタンと新規登録ボタンが表示されない', () => {
         console.log(wrapper.html());
